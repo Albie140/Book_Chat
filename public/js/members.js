203e8c654e5a652aa25d2fa8a8226999cd923c21
FileOtter <kevin.t.connell@gmail.com>
2020-10-13 14:53:18 -0400

    search works

--- public/js/members.js
+++ public/js/members.js
@@ -1,7 +1,32 @@
 $(document).ready(() => {
-  // This file just does a GET request to figure out which user is logged in
-  // and updates the HTML on the page
-  $.get("/api/user_data").then(data => {
-    $(".member-name").text(data.email);
+
+  const searchForm = $("form.search");
+  const searchInput = $("input#search_input");
+
+  searchForm.on("submit", event => {
+    event.preventDefault();
+
+    const searchTerm = searchInput.val().trim()
+
+    if (!searchTerm) {
+      return;
+    }
+
+    var queryURL = "https://www.googleapis.com/books/v1/volumes?q=" + searchTerm
+
+    // Ajax Function
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function (response) {
+      console.log(response);
+    });
+
+    // This file just does a GET request to figure out which user is logged in
+    // // and updates the HTML on the page
+    // $.get("/api/user_data").then(data => {
+    //   $(".member-name").text(data.username);
+    // });
   });
 });
