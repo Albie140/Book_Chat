b543d50972ee6f8cc34c368c111c94fb5f042681
FileOtter <kevin.t.connell@gmail.com>
2020-10-13 16:51:20 -0400

    added Add button, returns new query URL

--- public/js/members.js
+++ public/js/members.js
@@ -40,7 +40,7 @@ $(document).ready(() => {
     let rawHTML = "";
 
     for (let i = 0; i < 8; i++) {
-      rawHTML += `<div><img src="${data.items[i].volumeInfo.imageLinks ? data.items[i].volumeInfo.imageLinks.thumbnail : 'https://via.placeholder.com/128x196?text=No+Image+Found'}" alt="book cover"><h4>${data.items[i].volumeInfo.title ? data.items[i].volumeInfo.title : "No Title"}</h4><p>by ${data.items[i].volumeInfo.authors ? data.items[i].volumeInfo.authors.join(", ") : "No Author"}</p><p>${data.items[i].searchInfo ? data.items[i].searchInfo.textSnippet : "no description available"}</p></div>`;
+      rawHTML += `<div><img src="${data.items[i].volumeInfo.imageLinks ? data.items[i].volumeInfo.imageLinks.thumbnail : 'https://via.placeholder.com/128x196?text=No+Image+Found'}" alt="book cover"><h4>${data.items[i].volumeInfo.title ? data.items[i].volumeInfo.title : "No Title"}</h4><p>by ${data.items[i].volumeInfo.authors ? data.items[i].volumeInfo.authors.join(", ") : "No Author"}</p><p>${data.items[i].searchInfo ? data.items[i].searchInfo.textSnippet : "no description available"}</p><button class="addBook btn btn-default" type="submit" data-selfLink="${data.items[i].selfLink}">Add Club</button></div>`;
 
       console.log(rawHTML);
     };
@@ -51,4 +51,12 @@ $(document).ready(() => {
 
     console.log("done")
   }
+
+  $(".addBook").on("click", event => {
+    event.preventDefault();
+
+    console.log(event.target)
+
+    console.log($(event.target).attr("data-selfLink"));
+  })
 });
