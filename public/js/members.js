d1e75719524f03e4035fe0733a36a7436333263b
FileOtter <kevin.t.connell@gmail.com>
2020-10-13 16:08:38 -0400

    search results display

--- public/js/members.js
+++ public/js/members.js
@@ -21,6 +21,7 @@ $(document).ready(() => {
       method: "GET"
     }).then(function (response) {
       console.log(response);
+      renderSearch(response);
     });
 
     // This file just does a GET request to figure out which user is logged in
@@ -29,4 +30,25 @@ $(document).ready(() => {
     //   $(".member-name").text(data.username);
     // });
   });
+
+  function renderSearch(data) {
+
+    console.log("rendering search")
+
+    $("#bookResults").empty()
+
+    let rawHTML = "";
+
+    for (let i = 0; i < 8; i++) {
+      rawHTML += `<div><img src="${data.items[i].volumeInfo.imageLinks ? data.items[i].volumeInfo.imageLinks.thumbnail : 'https://via.placeholder.com/128x196?text=No+Image+Found'}" alt="book cover"><h4>${data.items[i].volumeInfo.title ? data.items[i].volumeInfo.title : "No Title"}</h4><p>by ${data.items[i].volumeInfo.authors ? data.items[i].volumeInfo.authors.join(", ") : "No Author"}</p><p>${data.items[i].searchInfo ? data.items[i].searchInfo.textSnippet : "no description available"}</p></div>`;
+
+      console.log(rawHTML);
+    };
+
+    console.log(rawHTML);
+
+    $("#bookResults").html(rawHTML);
+
+    console.log("done")
+  }
 });
