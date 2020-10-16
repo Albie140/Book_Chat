2047bdf2d7d521ff7f4ab82087cb081ae22d9c0d
andbret <and.bretnall@gmail.com>
2020-10-16 12:18:49 -0400

    responsive homepage

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -122,4 +122,19 @@ $(document).ready(() => {
     });
 
 });
+$.fn.stars = function() {
+  return $(this).each(function() {
+      // Get the value
+      var val = parseFloat($(this).html());
+      // Make sure that the value is in 0 - 5 range, multiply to get width
+      var size = Math.max(0, (Math.min(5, val))) * 18;
+      // Create stars holder
+      var $span = $('<span />').width(size);
+      // Replace the numerical value with stars
+      $(this).html($span);
+  });
+}
+$(function() {
+$('span.stars').stars();
+});
 //   var stupid = `type="submit" data-selfLink="${data.items[i].selfLink}"`
