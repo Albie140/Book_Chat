0a47018e2df5f007c4ec647acea7fa453e1aa02f
Alberte Laventure <alberte.laventure@gmail.com>
2020-10-16 10:15:18 -0400

    Deploying to Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,25 @@
 var Sequelize = require("sequelize");
 var mysql = require("mysql");
+const { config } = require("dotenv/types");
 
 
-var connection;
 
-if (process.env.JAWSDB_URL) {
-connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-
-connection = new Sequelize("bookchat_sequelize", "root", process.env.DB_PASS, {
-    host: "localhost",
-    port: process.env.PORT || 8080,
-    dialect: "mysql",
-    pool: {
-        max: 5,
-        min: 0,
-        idle: 10000 
-    }
-});
+if (config.use_env_variable) {
+    var connection =new Sequelize(process.env[cinfig.use_env_variable]);
+} else{
+    var connection = new Sequelize(config.database, config.username, config.password, config);
 }
+
+// connection = new Sequelize("bookchat_sequelize", "root", process.env.DB_PASS, {
+//     host: "localhost",
+//     port: process.env.PORT || 8080,
+//     dialect: "mysql",
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         idle: 10000 
+//     }
+// });
+
 
 module.exports = connection;
